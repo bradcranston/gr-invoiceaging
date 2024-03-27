e810503673e1d4c4cc6fefe47103318b450737fd
Bradley Cranston <brad@cranstoninc.com>
2024-03-27 12:19:15 -0400

    Version 1

---
+++ src/data.js
@@ -0,0 +1,5 @@
+const data =
+
+[[5117,1951,1050,"9/8/2009","Z8UADJBS9SJHLR3LRWJXGD","1-29",5313,"Gaskets Rock of Western Pennsylvania","Bob Evans #0422 - Uniontown","",348,348,1,6]]
+
+export default data ;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,153 @@
 //Here we're importing items we'll need. You can add other imports here.
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
-};
+
+import data from "./data";
+
+console.log(data) ;
+
+var table = new DataTable('#example', {
+
+
+
+
+
+
+
+
+    footerCallback: function (row, data, start, end, display) {
+        let api = this.api();
+ 
+        // Remove the formatting to get integer data for summation
+        let intVal = function (i) {
+            return typeof i === 'string'
+                ? i.replace(/[\$,]/g, '') * 1
+                : typeof i === 'number'
+                ? i
+                : 0;
+        };
+ 
+        // Total over all pages
+        balance = api
+            .column(10, { page: 'current' })
+            .data()
+            .reduce((a, b) => intVal(a) + intVal(b), 0);
+ 
+        // Total over this page
+        total = api
+            .column(11, { page: 'current' })
+            .data()
+            .reduce((a, b) => intVal(a) + intVal(b), 0);
+ 
+        // Update footer
+        api.column(10).header().innerHTML =
+            'Balance $' + balance.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",") ; 
+
+            // Update footer
+            api.column(11).header().innerHTML =
+                'Total $' + total.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",") ;
+    },
+
+
+
+
+
+
+
+
+
+
+    language: {
+        searchPanes: {
+            count: '{total} found',
+            countFiltered: '{shown} / {total}'
+        }
+    },  
+    paging: false,
+    data: data,
+    scrollY: '500px',
+    scrollCollapse: true,
+    columnDefs: [
+        {
+            searchPanes: {
+                show: false
+            },
+            title:'Inv',
+            targets: [0]
+        },{
+            searchPanes: {
+                show: false
+            },
+            title:'Cus',
+            targets: [1]
+        },{
+            searchPanes: {
+                show: false
+            },
+            title:'Loc',
+            targets: [2]
+        },{
+            searchPanes: {
+                show: false
+            },
+            title:'Date',
+            targets: [3]
+        },{
+            searchPanes: {
+                show: false
+            },
+            title:'bgId',
+            targets: [4],
+            visible: false
+        },{
+            searchPanes: {
+                show: true
+            },
+            title:'Aging',
+            targets: [5]
+        },{
+            searchPanes: {
+                show: false
+            },
+            title:'Days',
+            visible: false,
+            targets: [6]
+        },{
+            searchPanes: {
+                show: true
+            },
+            title:'Customer Name',
+            targets: [7]
+        },{
+            searchPanes: {
+                show: true
+            },
+            title:'Location Name',
+            targets: [8]
+        },{
+            searchPanes: {
+                show: true
+            },
+            title:'Reps',
+            targets: [9]
+        },{
+            searchPanes: {
+                show: false
+            },
+            title:'Balance',
+            targets: [10],
+            render: DataTable.render.number( null, null, 2, '$' )
+        },{
+            searchPanes: {
+                show: false
+            },
+            title:'Total',
+            targets: [11],
+            render: DataTable.render.number( null, null, 2, '$' )
+        }
+    ],
+    layout: {
+        top1: 'searchPanes'
+    }
+        }
+    
+);
